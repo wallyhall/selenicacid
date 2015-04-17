7ebcebd80f182cb55e24933f372ba17dfe74c4e4
Matt Hall <mhall@dev-27.dev.thinkfinanceuk.net>
2015-04-17 12:21:39 +0100

    - Added forking if available for httpd.  Should allow reasonable performance and better UI experience if added.

--- src/lib/Cli/HttpServer.php
+++ src/lib/Cli/HttpServer.php
@@ -28,14 +28,118 @@ class Cli_HttpServer
 			pcntl_signal(SIGINT, array("Cli_HttpServer", "handleSignal"));
 		}
 	
-		self::fork($user);
+        self::openPort($user);
+        self::readCmd();
 	}
 	
-	static function fork($user)
+	static function forkServer($user)
 	{
-		self::openPort();
-		self::readCmd();
-	}
+    }
+
+    static function forkRequest($conn)
+    {
+        $fork = -1;
+        $childPid = "";
+        if (PCNTL_ENABLED) {
+            $fork = pcntl_fork();
+            if ($fork == -1) {
+                fwrite(STDERR, "Failed to fork().  Continuing single-threaded.");
+            } elseif ($fork > 0) {
+                // allow parent to continue
+                fwrite(STDOUT, " -- Forked child #" . $fork . "\n");
+                fclose($conn);
+                return true;
+            } else {
+                fclose(self::$sock);
+                $childPid = getmypid();
+            }
+        }
+
+        // reference array for select()
+        $connArray = array($conn);
+        $emptyArray = array();
+
+        $requestFinished = false;
+        $requestLines = array();
+        while (!self::$shutdown && !$requestFinished && sizeof($requestLines) < 100) {
+            stream_select($connArray, $emptyArray, $emptyArray, 5);
+			if (false === ($buf = fgets($conn))) {
+				fwrite(STDERR, "Read failed.\n");
+				break;
+            }
+
+			$buf = trim($buf);
+			fwrite(STDOUT, "  $childPid<< $buf\n");
+				
+			if ($buf != "") {
+				$requestLines[] = $buf;
+			} else {
+				$requestFinished = true;
+            }
+		}
+
+		if (self::$shutdown) {
+			$response = "HTTP/1.0 500 Server shutting down.\n\nServer shutdown in progress.";
+			fwrite(STDERR, "Request aborted due to pending shutdown.\n");
+				
+		} elseif (!$requestFinished) {
+			$response = "HTTP/1.0 400 Bad request.\n\nBad request.";
+			fwrite(STDERR, "Request was rejected ... size: " . sizeof($requestLines) . "\n");
+				
+		} else {
+			// do stuff
+			$rawPath = null;
+			$rawParams = null;
+				
+			foreach ($requestLines as $requestLine) {
+				if (preg_match("/^(GET|POST)\s+(\S+)\s.*$/", $requestLine, $requestLineParts) === 1) {
+					$rawPath = parse_url($requestLineParts[2], PHP_URL_PATH);
+					$rawParams = parse_url($requestLineParts[2], PHP_URL_QUERY);
+					break;
+				}
+			}
+				
+			$response = "";
+			if ($rawPath === null) {
+				$response = "HTTP/1.0 400 Bad request.\n\nBad request.";
+				fwrite(STDERR, "Request appears malformed.\n");
+			} else {
+					
+				// URL HANDLING
+					
+				$module = substr(str_replace("/", "_", $rawPath), 1);
+				if ($module === false) {
+					$module = "Index";
+				}
+				
+				if ($rawParams === null) {
+					$assocParams = array();
+                } else {
+                    parse_str($rawParams, $assocParams);
+				}
+				
+				$output = Modules_Router::route($module, $assocParams, true);
+				if ($output === false) {
+					$response = "HTTP/1.0 404 Not Found.\n\nModule not found.";
+				} else {
+					$response = "HTTP/1.0 200 OK\n\n" . $output;
+				}
+			}
+		}
+
+        stream_select($emptyArray, $connArray, $emptyArray, 5);
+		fwrite($conn, $response, strlen($response));
+        fwrite(STDOUT, "  $childPid>> " . str_replace("\n", "\n  $childPid>> ", "$response\n"));
+
+        fclose($conn);
+
+        if ($fork == 0) {
+            // child exits
+            exit(0);
+        } else {
+            return false;
+        }
+    }
 	
 	static function openPort()
 	{
@@ -54,86 +158,16 @@ class Cli_HttpServer
             }
             stream_set_blocking($conn, 0);
 
-            // reference array for select()
-            $connArray = array($conn);
-            $emptyArray = array();
+            // Exit point for children.  Parent continues.
+            // If fork fails or isn't available, this function will block.
+            self::forkRequest($conn);
+        }
 
-            $requestFinished = false;
-            $requestLines = array();
-            while (!self::$shutdown && !$requestFinished && sizeof($requestLines) < 100) {
-                stream_select($connArray, $emptyArray, $emptyArray, 5);
-				if (false === ($buf = fgets($conn))) {
-					fwrite(STDERR, "Read failed.\n");
-					break;
-                }
-
-				$buf = trim($buf);
-				fwrite(STDOUT, "  << $buf\n");
-				
-				if ($buf != "") {
-					$requestLines[] = $buf;
-				} else {
-					$requestFinished = true;
-                }
-			}
-
-			if (self::$shutdown) {
-				$response = "HTTP/1.0 500 Server shutting down.\n\nServer shutdown in progress.";
-				fwrite(STDERR, "Request aborted due to pending shutdown.\n");
-				
-			} elseif (!$requestFinished) {
-				$response = "HTTP/1.0 400 Bad request.\n\nBad request.";
-				fwrite(STDERR, "Request was rejected ... size: " . sizeof($requestLines) . "\n");
-				
-			} else {
-				// do stuff
-				$rawPath = null;
-				$rawParams = null;
-				
-				foreach ($requestLines as $requestLine) {
-					if (preg_match("/^(GET|POST)\s+(\S+)\s.*$/", $requestLine, $requestLineParts) === 1) {
-						$rawPath = parse_url($requestLineParts[2], PHP_URL_PATH);
-						$rawParams = parse_url($requestLineParts[2], PHP_URL_QUERY);
-						break;
-					}
-				}
-				
-				$response = "";
-				if ($rawPath === null) {
-					$response = "HTTP/1.0 400 Bad request.\n\nBad request.";
-					fwrite(STDERR, "Request appears malformed.\n");
-				} else {
-					
-					// URL HANDLING
-					
-					$module = substr(str_replace("/", "_", $rawPath), 1);
-					if ($module === false) {
-						$module = "Index";
-					}
-					
-					if ($rawParams === null) {
-						$assocParams = array();
-                    } else {
-                        parse_str($rawParams, $assocParams);
-					}
-					
-					$output = Modules_Router::route($module, $assocParams, true);
-					if ($output === false) {
-						$response = "HTTP/1.0 404 Not Found.\n\nModule not found.";
-					} else {
-						$response = "HTTP/1.0 200 OK\n\n" . $output;
-					}
-				}
-			}
-
-            stream_select($emptyArray, $connArray, $emptyArray, 5);
-			fwrite($conn, $response, strlen($response));
-			fwrite(STDOUT, "  >> " . str_replace("\n", "\n  >> ", "$response\n"));
-
-            fclose($conn);
+        if (PCNTL_ENABLED) {
+            pcntl_wait($status);
         }
         fclose(self::$sock);
-		
+
 		if (!self::$shutdown) {
 			fwrite(STDERR, "Adnormal exit.\n");
 			exit(128);
